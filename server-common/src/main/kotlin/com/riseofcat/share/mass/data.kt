266b5bd71ab2925f82cad9d89a21b2224d1a3494
d.avdeev <avdeev@tutu.ru>
2018-03-14 13:07:28 +0300

    mutable

--- server-common/src/main/kotlin/com/riseofcat/share/mass/data.kt
+++ server-common/src/main/kotlin/com/riseofcat/share/mass/data.kt
@@ -102,7 +102,7 @@ val Angle.gdxTransformRotation:Float get() = degrees
   override fun act(state:State,getCar:GetCarById) {
     val scl = 100f
     val car = getCar.getCar(id) ?: return //todo handle null ?
-    car.speed = car.speed.add(action.direction.xy().immutable().scale(scl))//todo maybe redundant copy
+    car.speed = car.speed.add(action.direction.xy().mutable().scale(scl))//todo maybe redundant copy
     val s = car.size/15+1
     if(car.size-s>=GameConst.MIN_SIZE) car.size = car.size-s
     state.reactive.add(Reactive(id,s,action.direction.add(Angle.degreesAngle(180f)).xy().scale(3f*scl).mutable(),car.pos.mutable()))
@@ -146,7 +146,7 @@ val Angle.gdxTransformRotation:Float get() = degrees
 
   fun tick():State {
     val iterateFun:(SpeedObject)->Unit = {o->
-      o.pos = o.pos.add(o.speed.immutable().scale(GameConst.UPDATE_S))
+      o.pos = o.pos.add(o.speed.mutable().scale(GameConst.UPDATE_S))
       if(o.pos.x>=width())
         o.pos.x = o.pos.x-width()
       else if(o.pos.x<0) o.pos.x = o.pos.x+width()
